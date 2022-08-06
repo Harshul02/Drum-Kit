e1ccb5e1aff98516d6010cf49cc9b5ae9762c90f
Harshul Jain <harshulgangwal02@gmail.com>
2022-08-06 13:48:33 +0530

    Added Background image of all buttons using css

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var n=document.querySelectorAll(".drum").length;
 for(var i=0; i<n; i++)
 {
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        alert("I am Clicked");
+        var audio=new Audio("sounds/tom-1.mp3");
+        audio.play();
     });
 }
 
