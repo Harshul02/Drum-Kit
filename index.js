9797afd3a7915c2dd17f8a160bfc90bf5681203e
Harshul Jain <harshulgangwal02@gmail.com>
2022-08-06 13:39:08 +0530

    Added Event Listner

---
+++ index.js
@@ -0,0 +1,10 @@
+var n=document.querySelectorAll(".drum").length;
+
+for(var i=0; i<n; i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        alert("I am Clicked");
+    });
+}
+
+
